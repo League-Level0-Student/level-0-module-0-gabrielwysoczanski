1562b8d68be3b42e460b0c1a8b89fa1aada92af2
root <root@c2953d73f7cc>
2021-06-09 23:57:05 +0000

    Flamming Ninja, Greeter,Madlibs

--- src/_02_code_flow/_4_flaming_ninja_star/FlamingNinjaStar.java
+++ src/_02_code_flow/_4_flaming_ninja_star/FlamingNinjaStar.java
@@ -10,36 +10,38 @@ public class FlamingNinjaStar {
 		int flameSize = 130;		//the length of the flaming arms
 		
 		// Make a new robot, and set it's pen down.
-
+Robot ui = new Robot(500,500);
+ui.penDown();
 		// Set the robot speed to 100
-		
+		ui.setSpeed(100);
 		// Set the robot window size to 800 x 800
-
+ui.setWindowSize(800,800);
 		//  COUNT. Make another int variable to count how many times the loop has repeated
 		//         Set its start value to zero.
-		
+		int count=0;
 	       //  LOOP. Start a while loop to repeat all of the code below ONE time (we will change this later)
-
+while(count<25) {
 			   // TURN RIGHT     Turn the robot 1/8 of a circle (hint: 360 degrees will turn a full circle)
-		
+		ui.turn (360/8);
 			   // MOVE           Move the robot 64 pixels
-
-			   // TURN LEFT      Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
-			
-			   // DRAW FLAME     Move the robot the distance in the variable flameSize
+ui.move(64);
+ui.turn(-40);	   // TURN LEFT      Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
+ui.setPenColor(91,178,198);			
+ui.move(flameSize);		   // DRAW FLAME     Move the robot the distance in the variable flameSize
 			
 				//               Turn the robot 170 degrees
-			
+ui.turn(170);			
 				//               Move the robot the distance in the variable flameSize (again)
-			
+ui.move(flameSize);	
+ui.setPenColor(0,0,0);		
 			   // TURN RIGHT     Turn the robot 64 degrees to the right
-			
+	ui.turn(64);		
 				// MOVE         Move the robot the distance in the variable baseSize
-		
+		ui.move(baseSize);
 				//  INCREASE COUNT. Increase the count by 1
-		
+		count+=1;
 		//  End the while loop here
-		
+}
 		// TEST   Run the program. Check that your shape is the same as the first picture in the recipe. 
 		//        This is one arm of the ninja star.
 		
