120a94bb0a97a706107e8a92b9edd7c4a4a35b3d
root <root@b90d0de90a98>
2021-06-17 00:01:35 +0000

    Finish Up Quiz Game

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,11 +8,17 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+		String yesorno= JOptionPane.showInputDialog ("Do you know how to write code?  Yes or No?");
 		// 2. If they say "yes", tell them they will rule the world.
-
+if(yesorno .equals("yes")){ 
+	JOptionPane.showMessageDialog(null,"Rule the World!!! "); 
+	
+}
 		// 3. Otherwise, tell them to sign up for classes at The League.
+if(yesorno.equals("no")) {
+	JOptionPane.showMessageDialog(null,"Sign Up for classes at The League!!! ");
 
+}
 	}
 }
 
--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,23 +1,41 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
-		
-		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+
+		// Create a variable to hold the user's score. Set it equal to zero.
+		int duo = 0;
 		// ASK A QUESTION AND CHECK THE ANSWER
+		//
+		// 2. Ask the user a question
+		String yesorno = JOptionPane.showInputDialog("What is the Capital of New Jersey?  ");
+		// 3. Use an if statement to check if their answer is correct
+
+		// 4. if the user's answer was correct, add one to their score
+		if (yesorno.equals("Trenton")) {
+			JOptionPane.showMessageDialog(null, ("Correct"));
+			duo= duo+1;
+		}
+		else { 
+			JOptionPane.showMessageDialog(null, ("Incorrect"));
+			duo=duo-1;
+		}
+			
 		
-				// 2.  Ask the user a question 
-				
-				// 3.  Use an if statement to check if their answer is correct
-				
-				// 4.  if the user's answer was correct, add one to their score 
-		
-		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
-				// Option: Subtract a point from their score for a wrong answer
-		
-		
-		// After all the questions have been asked, tell the user their final score 
-		
+		// MAKE MORE QUESTIONS. Ask more questions by repeating the above
+		// Option: Subtract a point from their score for a wrong answer
+		yesorno = JOptionPane.showInputDialog("Do you know how to play a board game?  Yes or No?");
+		if (yesorno.equals("yes")) {
+			JOptionPane.showMessageDialog(null, ("-1"));
+
+		}
+		// After all the questions have been asked, tell the user their final score
+		{
+			JOptionPane.showMessageDialog(null, ("Final Score = 0"));
+
+		}
 	}
+
 }
